3d90a8fdbceb25bcc8d2be3d62d07a731efae87e
kaishaoshao <2032751077@qq.com>
2024-07-01 00:15:37 +0800

    test 2

--- src/ch_1/testfile.ll
+++
@@ -1,5 +0,0 @@
-define i32 @test1(i32 %A) {
-    %B = add i32 %A, 0
-    ret   i32 %B
-}
-
---
+++ src/ch_1/testfile/testfile.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# opt工具来进行一个优化————>指令合并
+opt -S -instcombine testfile.ll -o /output/output.ll
+
