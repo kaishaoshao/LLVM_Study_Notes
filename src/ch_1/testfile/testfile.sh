92de4ae5d94af722c3987f1e4b7f011aa0ab63c1
kaishaoshao <2032751077@qq.com>
2024-08-25 16:46:43 +0800

    test

--- src/ch_1/testfile/testfile.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# opt工具来进行一个优化————>指令合并
-opt -S -instcombine testfile.ll -o ../output/output.ll
-
-# 使用opt工具进行无用参数消除（dead-argument-elimination）优化：
-opt -S -deadargelim testfile.ll -o ../output/output2.ll
