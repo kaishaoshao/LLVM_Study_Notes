86e04a6c9d550523ff42a86127b6c8db561d1abe
kaishaoshao <2032751077@qq.com>
2024-11-16 22:41:12 +0800

    toy后端第一次提交---build toy target
    https://github.com/llvm/llvm-project/compare/main...sunwayforever:llvm-toy:sunway/toy

---
+++ src/toy_llvm/lib/Target/Toy/TargetInfo/ToyTargetInfo.cpp
@@ -0,0 +1,10 @@
+#include "llvm/IR/Module.h"
+#include "llvm/MC/TargetRegistry.h"
+
+using namespace llvm;
+Target TheToyTarget;
+
+extern "C" void LLVMInitializeToyTargetInfo() {
+  RegisterTarget<Triple::toy, true> X(TheToyTarget, "toy", "Toy RISC-V backend",
+                                      "Toy");
+}
